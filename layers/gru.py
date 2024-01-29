f1319c732e4903f8238d5e1f2858c347c14da0a4
Koral Kulacoglu <kulacoglukoral@gmail.com>
2024-01-29 13:59:25 -0500

    model template done

---
+++ layers/gru.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+# one single GRU layer
+class GRU:
+    def __init__(self, h_dim, h_new_dim, x_dim):
+        # initialize the following weights
+        # W_r, W_z, W, U_r, U_z, U, r_t, z_t, h_not_t
+
+    def forwprop():
+        # do stuff
+
+    def backprop():
+        # do stuff
